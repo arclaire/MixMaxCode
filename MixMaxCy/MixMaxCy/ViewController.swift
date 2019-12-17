e545c523cc26bfa70ae79d8ade75f7e22de385ea
Lucy <apptime.lucy@gmail.com>
2019-12-17 18:11:40 +0700

    first initial

---
+++ MixMaxCy/MixMaxCy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MixMaxCy
+//
+//  Created by Lucy on 17/12/19.
+//  Copyright © 2019 Lucy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
